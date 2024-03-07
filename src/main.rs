7b04506ded62294f390592efa090d01e2db7c4fa
AAbbey01 <aabbey1@binghamton.edu>
2024-03-06 20:12:42 -0500

    Updated Documentation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-//! Sample Stuff for the Crate Stuff
+//! Learning Rust: Project for CS471 at SUNY Binghamton
 //! ## Detailed Introduction
 //! This is a detailed intro to this libraray
-//! - [x] This Crate builds a fully operational binary search tree
-//! - [x] When run normally, users will be prompted to what they want to do with a tree
+//! - This Crate builds a fully operational binary search tree
+//! - When run normally, users will be able to interract with the terminal to create and modify the tree
 //! - Users will be able to use functions such as:
 //!  - [x] adding nodes to a tree
 //!  - [ ] deleting nodes
@@ -17,36 +17,38 @@
 //! tree.print();
 //! ```
 //! 🦀⚠️
-
 #![recursion_limit = "131072"]
 #![allow(non_snake_case)]
 //TODO: Add LR actual function to print
 use std::io;
+
+use BinaryTree::BinTree;
 //Node Stuff
 
-
+pub mod Nodes{
 pub struct Node{
-    val: i64,
+    pub(crate) val: i64,
 }
 impl Node{
     
-    fn print(&self){
+    pub fn print(&self){
         print!("{}\n", self.val);
     }
-
 }
-
+}
 //Binary Tree Stuff
+pub mod BinaryTree{
+    use crate::Nodes;
 
-struct BinTree{
-    root: Node,
-    left: Option<Box<BinTree>>,
-    right: Option<Box<BinTree>>,
+pub struct BinTree{
+    pub(crate) root: crate::Nodes::Node,
+    pub(crate) left: Option<Box<BinTree>>,
+    pub(crate) right: Option<Box<BinTree>>,
     
 }
 impl BinTree{
     
-    fn find(&self, key: i64) -> bool{
+    pub fn find(&self, key: i64) -> bool{
         //print!("{} is the current root\n", self.root.val);
         if self.root.val == key{
             
@@ -63,7 +65,7 @@ impl BinTree{
         }
         return false;
     }
-    fn add_node(&mut self, node: Node){
+    pub fn add_node(&mut self, node: crate::Nodes::Node){
         //print!("Adding to {}\n",self.root.val);
         if node.val == -1 {return;}
         if self.root.val == -1 {
@@ -86,7 +88,7 @@ impl BinTree{
             }
         }
     }
-    fn print(&self){
+    pub fn print(&self){
         if self.root.val == -1 {return;}
         //unsafe { self.left.as_ref().unwrap_unchecked().print() };
         //unsafe { self.right.as_ref().unwrap_unchecked().print() };
@@ -107,7 +109,7 @@ impl BinTree{
             
         }
     }
-    fn add_next(&mut self, node: Node){
+    fn add_next(&mut self, node: Nodes::Node){
         if node.val == -1 {return;}
         if self.root.val == -1 {
             self. root = node;
@@ -129,7 +131,7 @@ impl BinTree{
         }
     }
     
-    fn delete( &mut self, node: i64){
+    pub fn delete( &mut self, node: i64){
         //Check if the value to be deleted is in the tree in the first place
         if !self.find(node){
             println!("Value not in tree");
@@ -142,13 +144,13 @@ impl BinTree{
 #[warn(unconditional_recursion)]
 impl Default for BinTree{
     fn default() -> Self {
-        BinTree{root: Node{val: -1}, left: None, right: None}
+        BinTree{root: Nodes::Node{val: -1}, left: None, right: None}
     }
 }
-
+}
 fn main(){
     //n.print();
-    let mut b_t = BinTree{..Default::default()};
+    let mut b_t = BinaryTree::BinTree{..Default::default()};
     loop{
         print!("Functions (Type the Letter for Each)\nA: Add a Key to the Tree (Until -1 is inputted)\nF: Find if a Key is in the Tree\nD: Delete A Key in the Tree\nP: Print Tree\nE: Exit (Ctrl+C to Force Stop)\n");
         let mut power_input = String::new();
@@ -226,6 +228,6 @@ fn add_node_public(n: &mut BinTree){
         numbers.push(num);
     }
     for num in numbers {
-        n.add_node(Node{val: num});
+        n.add_node(Nodes::Node{val: num});
     }
 }
